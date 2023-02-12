b86d44b506e87792b6f6d7942a9e2ddf77b65342
George Paredes <119899081+paredesg27@users.noreply.github.com>
2023-02-11 20:01:01 -0500

    Update OfficeDetailFragment.kt

--- app/src/main/java/com/example/csc415base/ui/OfficeDetailFragment.kt
+++ app/src/main/java/com/example/csc415base/ui/OfficeDetailFragment.kt
@@ -1,27 +1,42 @@
 package com.example.csc415base.ui
 
+
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import com.example.csc415base.R
+import com.bumptech.glide.Glide
+import com.example.andriodapptest.R
 
 
 class OfficeDetailFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_office_detail, container, false)
 
         if (arguments != null){
-            val name = requireArguments().getString("name")
-            val age = requireArguments().getString("age")
+            val officeDescriptions = listOf("0","1","2","3","4","5","6","7","8","9","10","10","12")
+
+            val name ="Name: " + requireArguments().getString("name")
+            val age = "Age: " + requireArguments().getInt("age").toString()
+            val job = "Job: " + requireArguments().getString("job")
+            val image = requireArguments().getString("image")
+            val officeId = requireArguments().getInt("id")
             view.findViewById<TextView>(R.id.office_age).text = age
+            view.findViewById<TextView>(R.id.office_job).text = job
+            view.findViewById<TextView>(R.id.office_description).text = officeDescriptions[officeId]
+            view.findViewById<TextView>(R.id.office_name).text = name
+            Glide.with(requireContext()).load(image).override(1000).into(view.findViewById(R.id.office_image))
+
+
         }
 
         return view
