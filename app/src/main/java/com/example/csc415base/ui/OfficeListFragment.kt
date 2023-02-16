6cdb1baebc7d1769b839670422609a1bee7c966e
George Paredes <119899081+paredesg27@users.noreply.github.com>
2023-02-15 21:02:05 -0500

    Update OfficeListFragment.kt

--- app/src/main/java/com/example/csc415base/ui/OfficeListFragment.kt
+++ app/src/main/java/com/example/csc415base/ui/OfficeListFragment.kt
@@ -14,8 +14,9 @@ import com.example.csc415base.R
 import kotlin.random.Random
 
 class OfficeListFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+   override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
@@ -57,13 +58,29 @@ class OfficeListFragment : Fragment() {
             "Ex-Human Resources",
             "Salesman"
         )
+        val listImages = listOf(
+            "https://d1zp01vk4nyewr.cloudfront.net/images/blog/_1200x630_crop_center-center_82_none/scott.jpg?mtime=1622137257",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4TjuN6VTO5xNRCHyPdHm1Gf1CAXSwRjRpvg&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTppTjguozjXk3WRtXC_UFP_1-LuSElPdn0sg&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRspbfZiL1UHmEfSxyEa3FzGMZl0P4GpeQaw&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTy1L3tFWAq93Y3RPJzjI7WFdFEHe2f_OKepg&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaxtWozMqJDG1PkdTEZTAjhnTu8jQ-KOgx4A&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfWX794Vk1oZQt_c2KLHnNU7dCHVGwM-OArg&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrSOTQQSRTkjzwEk3MvceivY6PyAAFPB4T2w&usqp=CAU",
+            "https://www.looper.com/img/gallery/randall-park-forgetting-about-his-role-on-the-office-led-to-some-awkward-fan-encounters/intro-1669938075.jpg",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzZppt6MXH_A_FIu0KVXcK93Xwb3Rd632Uig&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiFIHUk9khEnuhFy3CBS0iCMpbg5P-NsTBTQ&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiqBguHfwzVmhlz4H6QJpO9rkGaf4pL7Y60g&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPq5O8om43i0AfRRLI4gFuf0F9a5OtGpV-Sw&usqp=CAU",
+
+            )
 
         for (i in 0..12) {
             characters.add(
                 createCharacter(
                     listNames[i],
                     Random.nextInt(30, 60),
-                    "https://d1zp01vk4nyewr.cloudfront.net/images/blog/_1200x630_crop_center-center_82_none/scott.jpg?mtime=1622137257",
+                    listImages[i],
                     listJobs[i],
                     i,
                     companiesList
